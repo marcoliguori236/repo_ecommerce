0a81eb8bb0890147b0e4808f322a0983be7d5908
Marco Liguori <40567641+marcoliguori236@users.noreply.github.com>
2020-08-09 13:32:28 -0300

    entrega1_parte2
    Realizar una petición web a una URL donde se encuentra una colección de productos en formato JSON, con la información básica (precio, nombre, breve descripción) respectiva a cada producto, y mostrar el listado en HTML.

--- js/products.js
+++ js/products.js
@@ -1,6 +1,47 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var productsArray = [];
 
+function showProductsList(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.cost + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                    <div class="mb-1">
+                        <h4>`+ product.name + `</h4>
+                        <p>`+ product.description + `</p>
+                    </div>    
+                        <small class="text-muted">` + product.soldCount + ` artículos</small>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showProductsList(productsArray);
+        }
+    });
 });
