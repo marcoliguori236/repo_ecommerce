c8f0aba9993029f8429ef66669e8f9e7603c96a8
Marco Liguori <40567641+marcoliguori236@users.noreply.github.com>
2020-08-09 18:01:12 -0300

    entrega1_p3y4
    p3)Cambio login.html por ejemplo de login en bootstrap.
    p4)Creo función loginRedirect para enviar al usuario a index.html cuando clickea Submit

--- js/login.js
+++ js/login.js
@@ -1,6 +1,10 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+function loginRedirect() {
+    window.open("file:///C:/Users/marco/Documents/GitHub/repo_ecommerce/index.html")
+  }
+
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
