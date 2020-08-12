135884737e2eccaba66dcef1f8f1d118f41a6ca3
Marco Liguori <marcoliguori236@gmail.com>
2020-08-12 09:53:29 -0300

    ajustes entrega1_p2y3

--- js/login.js
+++ js/login.js
@@ -1,10 +1,6 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-function loginRedirect() {
-  window.open("file:///C:/Users/marco/Documents/GitHub/repo_ecommerce/index.html")
-}
-
 
 //autenticacion google
 function onSignIn(googleUser) {
