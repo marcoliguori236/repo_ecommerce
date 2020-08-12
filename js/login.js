f0ac3f58b74d3da9bd9ad4f2a51791c6699ec53b
Marco Liguori <40567641+marcoliguori236@users.noreply.github.com>
2020-08-12 08:09:09 -0300

    autenticacion google a remover para entrega1

--- js/login.js
+++ js/login.js
@@ -2,9 +2,28 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 function loginRedirect() {
-    window.open("file:///C:/Users/marco/Documents/GitHub/repo_ecommerce/index.html")
-  }
+  window.open("file:///C:/Users/marco/Documents/GitHub/repo_ecommerce/index.html")
+}
 
-document.addEventListener("DOMContentLoaded", function(e){
+
+//autenticacion google
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+//autenticacion google
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
